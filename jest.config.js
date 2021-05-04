b859c0401126fd3f9855d05bfd7cbb8a729648c5
Lenz Weber <mail@lenzw.de>
2021-05-04 18:11:59 +0200

    test

--- jest.config.js
+++ jest.config.js
@@ -2,15 +2,25 @@ module.exports = {
   setupFilesAfterEnv: ['./jest.setup.js'],
   testPathIgnorePatterns: ['<rootDir>/query-old/*'],
   moduleNameMapper: {
-    '^@reduxjs/toolkit$': '<rootDir>/src/index.ts', // @remap-prod-remove-line
-    '^@reduxjs/toolkit/query$': '<rootDir>/src/query/index.ts', // @remap-prod-remove-line
-    '^@reduxjs/toolkit/query/react$': '<rootDir>/src/query/react/index.ts', // @remap-prod-remove-line
+    ...(process.env.CI == 'true'
+      ? {}
+      : {
+          '^@reduxjs/toolkit$': '<rootDir>/src/index.ts',
+          '^@reduxjs/toolkit/query$': '<rootDir>/src/query/index.ts',
+          '^@reduxjs/toolkit/query/react$':
+            '<rootDir>/src/query/react/index.ts',
+        }),
     // this mapping is disabled as we want `dist` imports in the tests only to be used for "type-only" imports which don't play a role for jest
     //'^@reduxjs/toolkit/dist/(.*)$': '<rootDir>/src/*',
     '^@internal/(.*)$': '<rootDir>/src/$1',
   },
+  preset:
+    process.env.ESM == 'true'
+      ? 'ts-jest/presets/default-esm'
+      : 'ts-jest/presets/default',
   globals: {
     'ts-jest': {
+      useESM: process.env.ESM == 'true',
       tsconfig: 'tsconfig.test.json',
       diagnostics: {
         ignoreCodes: [6133],
